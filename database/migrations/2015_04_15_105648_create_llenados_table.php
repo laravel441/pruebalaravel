cb6db142e238cb32a39c72e34e32a9178891049f
unknown <william.uricoechea@TIC-IVANGAR.masivocapital.com>
2015-04-15 11:10:45 -0500

    Migraciones

---
+++ database/migrations/2015_04_15_105648_create_llenados_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLlenadosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('llenados', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('cilindro_id')->unsigned();
+			$table->integer('tercero_id')->unsigned();
+			$table->enum('estado',['llenado','lleno']);
+			$table->string('notas',1000)->nullable;
+   			$table->timestamps();
+
+   			$table->foreign('tercero_id')
+   				  ->references('id')->on('terceros')
+   				  ->onUpdate('CASCADE')
+   				  ->onDelete('CASCADE');
+   				  
+   			$table->foreign('cilindro_id')
+   				  ->references('id')->on('cilindros')
+   				  ->onUpdate('CASCADE')
+   				  ->onDelete('CASCADE');
+
+   			
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('llenados');
+	}
+
+}
